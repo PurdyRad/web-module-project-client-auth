8cf1e5e2c260b4f0ce515f1f4b0c87ec57008057
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-03-23 21:55:27 -0400

    done

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// const initialRealOne = [{
+//     id: 0,
+//     name: '',
+//     age: 27,
+//     email: '',
+// }]
+
+const Friend = () => {
+    // const[realOne, setRealOne]=useState(initialRealOne)
+
+    // useEffect(() =>{
+    //     clicka();
+    // }, [])
+
+    // const clicka = () => {
+    //     axios.get('http://localhost:5000/api/friends', 
+    //     {headers: {
+    //         authorization: localStorage.getItem('token')
+    //     }})
+    //     .then(res => {
+    //         setRealOne({
+    //             realOne: res.data
+    //         });
+    //     })
+    //     .catch(err => {console.log('err:', err.response)})
+    //     }
+    
+    return (
+        <div>
+            
+            
+        </div>
+    )
+}
+
+export default Friend
